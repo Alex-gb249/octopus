b2d18cb9165cbbde2ba3176095510e1f137dad35
Bryan Alexis Guzmán Bermúdez <baguzman897@misena.edu.co>
2021-10-20 14:33:16 -0500

    Código más bonito

--- Octopus/classifyPDF.py
+++ Octopus/classifyPDF.py
@@ -11,10 +11,45 @@ directorios = {
     "images":os.path.dirname(os.path.abspath(__file__))+"\\images"
 }
 
-lista_pdfs = os.listdir(directorios["pdfs"])
+lista_pdfs = os.listdir(directorios["pdfs"]) # Lista con los nombres de los archivos.
 
 if lista_pdfs:
-    for x in lista_pdfs:
-        print(x)
+    for pdf in lista_pdfs:
+        try:
+            if pdf.split(".")[-1] == "pdf": # Esto comprueba si el documento es formato pdf, esto se puede cambiar/reemplazar/eliminar si se requiere
+                print("Empezando la clasificación...")
+                inicio = time.time() # Inicio toma del tiempo.
+                rutaPDF = directorios["pdfs"]+'\\'+pdf # Ruta del pdf.
+                images = convert_from_path(rutaPDF, last_page=1) #Rescata la página 1 del pdf
+                images[0].save(directorios["images"]+'\\'+'page'+str(0)+'.jpg', 'JPEG')
+
+                texto = pytesseract.image_to_string(directorios["images"]+"\\page0.jpg")
+
+                if texto:
+                    # Tiene texto
+                    if "factura" in texto:
+                        # Mover a la carpeta de facturas
+                        shutil.move(rutaPDF, directorios["categorias"]+"\\"+"Facturas")
+                    elif "cuenta de cobro" in texto:
+                        # En caso de cuenta de cobro
+                        shutil.move(rutaPDF, directorios["categorias"]+"\\"+"Cuentas de cobro")
+                    elif "sena" in texto:
+                        # En caso sea algo del sena XD
+                        shutil.move(rutaPDF, directorios["categorias"]+"\\"+"SENA")
+                    else:
+                        # En caso que no se identifique a donde pertenece
+                        shutil.move(rutaPDF, directorios["categorias"]+"\\"+"Otros")
+                    pass
+                else:
+                    # Significa que la primera página vino vacía o no se encontró texto
+                    print("No se encontró texto para clasificar. (En la primera página)")
+                    pass
+
+                fin = time.time()
+                print(f"Tiempo de clasificación para el pdf'{pdf}': {fin-inicio}")
+            else:
+                print(f"El archivo {pdf} no es extensión '.pdf'")
+        except:
+            pass
 else: 
-    print("Fokiu")
+    print("Carpeta vacía general vacía.")
